abccc2c0b52a5e492c69fbb4ba6016cdce1efbbf
fhildeb <felix@lukso.io>
2022-07-02 23:53:43 +0200

    add practical 8

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ma-blockchain
 
-HSMW Blockchain Course
+MA Blockchain Course from the University of Applied Science Mittweida that took place in 2020.
+
+NOTE: The commentary and filenames might appear in German.
---
+++ practical-8/DemoContract.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.7.5;
+
+contract lesson01{
+    string public name;
+
+    constructor(){
+
+    }
+
+    function helloWorld() public pure returns(string memory){
+        return "Hello World!";
+    }
+
+
+    function calculator(uint a, uint b) public pure returns(uint){
+        return a + b;
+    }
+
+    function storeName(string memory _name) public{
+        name = _name;
+    }
+
+    function payMe() public payable{
+        require(msg.value > 1000000);
+    }
+
+    function getBalacne() public view returns(uint){
+        return address(this).balance;
+    }
+
+}
