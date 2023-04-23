0f0999803ab723d294322ce9aed6059d58b1a34b
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-04-24 00:42:40 +0200

    update and fix write contract gas estimation

--- 12-cdn-dapps/writeContract.sol
+++ 12-cdn-dapps/writeContract.sol
@@ -1,12 +1,20 @@
 pragma solidity ^0.8.0;
 
-contract WriteContract{
+contract WriteContract {
     string public text;
-    uint256 public price = 100000000000000000;  // In Wei = 0,01 ETH
+    uint256 public price = 10000;
     uint256 public counter = 0;
+    address payable public owner;
 
-    function writeText(string memory _text) payable public{
+    constructor() {
+        owner = payable(msg.sender); // Set the contract owner to the address deploying the contract
+    }
+
+    function writeText(string memory _text) payable public {
         require(msg.value >= price, "Not enough ether");
         text = _text;
+
+        // Transfer the received ether to the contract owner
+        owner.transfer(msg.value);
     }
 }
