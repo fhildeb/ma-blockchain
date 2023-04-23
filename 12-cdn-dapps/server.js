8a871c4fb4a3b4543a54b66d1030f2adb159b004
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-04-24 00:00:43 +0200

    bugfix express to allow static styling

--- 12-cdn-dapps/server.js
+++ 12-cdn-dapps/server.js
@@ -2,8 +2,10 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
+app.use(express.static(path.join(__dirname, "./"))); // Serve static files from the current directory
+
 app.listen(3000); // Port
 app.get("/", function (req, res) {
   console.log("Listening to Port 3000");
-  res.sendFile(path.join(__dirname, "./", "dApp_Eth_Info.html")); //TODO: Adjust HTML Path
+  res.sendFile(path.join(__dirname, "./", "dApp_Transaction.html")); // Adjust the path if necessary
 });
