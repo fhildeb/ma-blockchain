ddcb23b1a6169d613da0fe261839c5fe0696ca23
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-04-23 23:44:10 +0200

    add new server file

--- 12-cdn-dapps/server.js
+++ 12-cdn-dapps/server.js
@@ -5,5 +5,5 @@ var app = express();
 app.listen(3000); // Port
 app.get("/", function (req, res) {
   console.log("Listening to Port 3000");
-  res.sendFile(path.join(__dirname, "./", "Aufgabe_2_Dapp_adding.html")); //Pfad ggf. anpassen
+  res.sendFile(path.join(__dirname, "./", "dApp_Eth_Info.html")); //TODO: Adjust HTML Path
 });
