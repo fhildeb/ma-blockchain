fa1dcf51c35eb9ebaa8a32dadc179eb842e46a2c
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-04-23 20:19:21 +0200

    improve balance search

--- 06-balance-search/search-balance.js
+++ 06-balance-search/search-balance.js
@@ -1,24 +1,28 @@
 //Crypto Library
 const crypto = require("crypto");
 
-//Address
+// Address
 const hash = "6a22abc984f8bed473a6fe7841cb520f879ebfa97f7b1ad0c24bcb2e7bc79428";
 
-//Maximum amount up to which the algorithm searches
-const max_amount = 10000000;
+// Maximum amount up to which the algorithm searches
+const total_searches = 10000000;
 
-//Algorithm to search for the balance on the address
-for (i = 0; i < max_amount; i++) {
-  var test_hash = crypto
+// Algorithm to search for the balance on the address
+let matchFound = false;
+for (let i = 0; i < total_searches; i++) {
+  let test_hash = crypto
     .createHash("RSA-SHA256")
     .update(i.toString(), "utf8")
     .digest("hex");
 
-  //If balance found
-  if (test_hash == hash) {
-    console.log("");
-    console.log("Match: Account balance is " + i.toString());
-    console.log("");
+  // If balance found
+  if (test_hash === hash) {
+    console.log("\nMatch: Account balance is " + i.toString() + "\n");
+    matchFound = true;
     break;
   }
+}
+
+if (!matchFound) {
+  console.log("\nNo match found within the specified range.\n");
 }
